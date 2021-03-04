ab05870e6ebe402a47b50a2cf7e955c2b22ec5f6
IlyaErgin <erginergin15@yandex.ru>
2021-03-04 22:00:57 +0300

    laboratory work: calendar on JS with React

---
+++ src/components/Title/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const DivWrapper = styled('div')`s
+`;
+const Title = () => (
+	<DivWrapper></DivWrapper>
+);
+
+export { Title };
